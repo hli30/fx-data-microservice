2f154935ee4e6d4741ff9bc82bd54ef3182b4506
Harry L <harrylll93@gmail.com>
2018-08-02 20:21:20 -0400

    initial commit, migrate from old project

---
+++ BrokerService.Libs/Scheduler/DailyTask.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrokerService.Libs.Scheduler
+{
+    public class DailyTask : ScheduleProcessor
+    {
+        //runs on the minute for testing
+        protected override string Schedule => "*/1 * * * *";
+        //runs Sun to Fri at 21:15 
+        //protected override string Schedule => "15 21 * * 0-5";
+
+        public IConfiguration Configuration { get; }
+
+        public DailyTask(IConfiguration configuration) : base()
+        {
+            Configuration = configuration;
+        }
+
+        protected override Task DailyCandleTask()
+        {
+            Console.WriteLine("Running daily candle task");
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ BrokerService.Libs/Scheduler/ScheduleProcessor.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using NCrontab;
+
+namespace BrokerService.Libs.Scheduler
+{
+    public abstract class ScheduleProcessor : BackgroundService
+    {
+        private CrontabSchedule _schedule;
+        private DateTime _nextRun;
+        protected abstract string Schedule { get; }
+
+        public ScheduleProcessor() : base()
+        {
+            _schedule = CrontabSchedule.Parse(Schedule);
+            _nextRun = _schedule.GetNextOccurrence(DateTime.Now);
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            int counter = 0;
+            Console.WriteLine("service started, do 1st time setup");
+
+            do
+            {
+                var now = DateTime.Now;
+
+                if (now > _nextRun)
+                {
+                    Console.WriteLine("Starting Task");
+                    await DailyCandleTask();
+                    _nextRun = _schedule.GetNextOccurrence(now);
+                    Console.WriteLine("Task complete, reset");
+                    counter = 0;
+                }
+                Console.WriteLine($"waiting {counter} sec");
+                await Task.Delay(10000, stoppingToken);
+                counter += 10;
+            }
+            while (!stoppingToken.IsCancellationRequested);
+        }
+
+        protected abstract Task DailyCandleTask();
+    }
+}
